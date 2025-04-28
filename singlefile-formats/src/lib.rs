ef0f9ca114b40b4d1a58f771d7ead9921062b9af
ScottyThePilot <scotty.codes@gmail.com>
2025-04-28 11:07:41 -0400

    Compression feature

--- singlefile-formats/src/lib.rs
+++ singlefile-formats/src/lib.rs
@@ -24,97 +24,103 @@
 
 pub extern crate singlefile;
 
-use singlefile::FileFormat;
+/// Defines a compression format interface, and a [`FileFormat`] which wraps another [`FileFormat`],
+/// is generic over compression formats, and compresses the contents of the wrapped format.
+#[cfg_attr(docsrs, doc(cfg(feature = "base64")))]
+#[cfg(feature = "compression")]
+pub mod compression {
+  use singlefile::FileFormat;
 
-use std::io::{Read, Write};
+  use std::io::{Read, Write};
 
-/// Combines a [`FileFormat`] and a [`CompressionFormat`], making the contents emitted by
-/// the format compressed before writing to disk, and decompressed before parsing.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Compressed<C, F> {
-  /// The [`FileFormat`] to be used.
-  pub format: F,
-  /// The [`CompressionFormat`] to be used.
-  pub compression: C,
-  /// The level of compression to use.
-  /// This value may have different meanings for different compression formats.
-  pub level: u32
-}
-
-impl<C, F> Compressed<C, F> {
-  /// Create a new [`Compressed`], given a compression level.
-  #[inline]
-  pub const fn with_level(format: F, compression: C, level: u32) -> Self {
-    Compressed { format, compression, level }
-  }
-}
-
-impl<C, F> Compressed<C, F> where C: CompressionFormatLevels {
-  /// Creates a new [`Compressed`] with the default compression level.
-  #[inline]
-  pub const fn new(format: F, compression: C) -> Self {
-    Compressed::with_level(format, compression, C::COMPRESSION_LEVEL_DEFAULT)
+  /// Combines a [`FileFormat`] and a [`CompressionFormat`], making the contents emitted by
+  /// the format compressed before writing to disk, and decompressed before parsing.
+  #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+  pub struct Compressed<C, F> {
+    /// The [`FileFormat`] to be used.
+    pub format: F,
+    /// The [`CompressionFormat`] to be used.
+    pub compression: C,
+    /// The level of compression to use.
+    /// This value may have different meanings for different compression formats.
+    pub level: u32
   }
 
-  /// Creates a new [`Compressed`] with the 'fast' compression level.
-  #[inline]
-  pub const fn new_fast_compression(format: F, compression: C) -> Self {
-    Compressed::with_level(format, compression, C::COMPRESSION_LEVEL_FAST)
+  impl<C, F> Compressed<C, F> {
+    /// Create a new [`Compressed`], given a compression level.
+    #[inline]
+    pub const fn with_level(format: F, compression: C, level: u32) -> Self {
+      Compressed { format, compression, level }
+    }
   }
 
-  /// Creates a new [`Compressed`] with the 'best' compression level.
-  #[inline]
-  pub const fn new_best_compression(format: F, compression: C) -> Self {
-    Compressed::with_level(format, compression, C::COMPRESSION_LEVEL_BEST)
-  }
-}
+  impl<C, F> Compressed<C, F> where C: CompressionFormatLevels {
+    /// Creates a new [`Compressed`] with the default compression level.
+    #[inline]
+    pub const fn new(format: F, compression: C) -> Self {
+      Compressed::with_level(format, compression, C::COMPRESSION_LEVEL_DEFAULT)
+    }
 
-impl<C, F> Default for Compressed<C, F>
-where C: Default + CompressionFormatLevels, F: Default {
-  #[inline]
-  fn default() -> Self {
-    Compressed::new(F::default(), C::default())
-  }
-}
+    /// Creates a new [`Compressed`] with the 'fast' compression level.
+    #[inline]
+    pub const fn new_fast_compression(format: F, compression: C) -> Self {
+      Compressed::with_level(format, compression, C::COMPRESSION_LEVEL_FAST)
+    }
 
-impl<T, C, F> FileFormat<T> for Compressed<C, F>
-where C: CompressionFormat, F: FileFormat<T> {
-  type FormatError = F::FormatError;
-
-  fn from_reader<R: Read>(&self, reader: R) -> Result<T, Self::FormatError> {
-    self.format.from_reader(self.compression.decode_reader(reader))
+    /// Creates a new [`Compressed`] with the 'best' compression level.
+    #[inline]
+    pub const fn new_best_compression(format: F, compression: C) -> Self {
+      Compressed::with_level(format, compression, C::COMPRESSION_LEVEL_BEST)
+    }
   }
 
-  fn to_writer<W: Write>(&self, writer: W, value: &T) -> Result<(), Self::FormatError> {
-    self.format.to_writer(self.compression.encode_writer(writer, self.level), value)
+  impl<C, F> Default for Compressed<C, F>
+  where C: Default + CompressionFormatLevels, F: Default {
+    #[inline]
+    fn default() -> Self {
+      Compressed::new(F::default(), C::default())
+    }
   }
-}
 
-/// Defines a format for lossless compression of arbitrary data.
-///
-/// In order to use a [`CompressionFormat`], you may consider using the [`Compressed`] struct.
-pub trait CompressionFormat {
-  /// The encoder wrapper type that compresses data sent to the contained writer.
-  type Encoder<W: Write>: Write;
-  /// The decoder wrapper type that decompresses data sent from the contained reader.
-  type Decoder<R: Read>: Read;
+  impl<T, C, F> FileFormat<T> for Compressed<C, F>
+  where C: CompressionFormat, F: FileFormat<T> {
+    type FormatError = F::FormatError;
 
-  /// Wraps a writer that takes uncompressed data, producing a new writer that outputs compressed data.
-  fn encode_writer<W: Write>(&self, writer: W, level: u32) -> Self::Encoder<W>;
-  /// Wraps a reader that takes compressed data, producing a new reader that outputs uncompressed data.
-  fn decode_reader<R: Read>(&self, reader: R) -> Self::Decoder<R>;
-}
+    fn from_reader<R: Read>(&self, reader: R) -> Result<T, Self::FormatError> {
+      self.format.from_reader(self.compression.decode_reader(reader))
+    }
 
-/// Defines compression level presets for a [`CompressionFormat`].
-pub trait CompressionFormatLevels: CompressionFormat {
-  /// The level for no compression.
-  const COMPRESSION_LEVEL_NONE: u32;
-  /// The level for 'fast' compression.
-  const COMPRESSION_LEVEL_FAST: u32;
-  /// The level for 'best' compression.
-  const COMPRESSION_LEVEL_BEST: u32;
-  /// The level for default compression.
-  const COMPRESSION_LEVEL_DEFAULT: u32;
+    fn to_writer<W: Write>(&self, writer: W, value: &T) -> Result<(), Self::FormatError> {
+      self.format.to_writer(self.compression.encode_writer(writer, self.level), value)
+    }
+  }
+
+  /// Defines a format for lossless compression of arbitrary data.
+  ///
+  /// In order to use a [`CompressionFormat`], you may consider using the [`Compressed`] struct.
+  pub trait CompressionFormat {
+    /// The encoder wrapper type that compresses data sent to the contained writer.
+    type Encoder<W: Write>: Write;
+    /// The decoder wrapper type that decompresses data sent from the contained reader.
+    type Decoder<R: Read>: Read;
+
+    /// Wraps a writer that takes uncompressed data, producing a new writer that outputs compressed data.
+    fn encode_writer<W: Write>(&self, writer: W, level: u32) -> Self::Encoder<W>;
+    /// Wraps a reader that takes compressed data, producing a new reader that outputs uncompressed data.
+    fn decode_reader<R: Read>(&self, reader: R) -> Self::Decoder<R>;
+  }
+
+  /// Defines compression level presets for a [`CompressionFormat`].
+  pub trait CompressionFormatLevels: CompressionFormat {
+    /// The level for no compression.
+    const COMPRESSION_LEVEL_NONE: u32;
+    /// The level for 'fast' compression.
+    const COMPRESSION_LEVEL_FAST: u32;
+    /// The level for 'best' compression.
+    const COMPRESSION_LEVEL_BEST: u32;
+    /// The level for default compression.
+    const COMPRESSION_LEVEL_DEFAULT: u32;
+  }
 }
 
 /// Defines a [`FileFormat`] that wraps data from another format in Base64.
@@ -247,7 +253,9 @@ pub mod cbor_serde {
 
   /// A shortcut type to a [`Compressed`][crate::Compressed] [`Cbor`].
   /// Provides a single parameter for compression format.
-  pub type CompressedCbor<C> = crate::Compressed<C, Cbor>;
+  #[cfg_attr(docsrs, doc(cfg(feature = "compression")))]
+  #[cfg(feature = "compression")]
+  pub type CompressedCbor<C> = crate::compression::Compressed<C, Cbor>;
 }
 
 /// Defines a [`FileFormat`] using the JSON data format.
@@ -316,7 +324,9 @@ pub mod json_serde {
 
   /// A shortcut type to a [`Compressed`][crate::Compressed] [`Json`].
   /// Provides parameters for compression format and pretty-print configuration (defaulting to off).
-  pub type CompressedJson<C, const PRETTY: bool = false> = crate::Compressed<C, Json<PRETTY>>;
+  #[cfg_attr(docsrs, doc(cfg(feature = "compression")))]
+  #[cfg(feature = "compression")]
+  pub type CompressedJson<C, const PRETTY: bool = false> = crate::compression::Compressed<C, Json<PRETTY>>;
 }
 
 /// Defines a [`FileFormat`] using the TOML data format.
@@ -408,7 +418,9 @@ pub mod toml_serde {
 
   /// A shortcut type to a [`Compressed`][crate::Compressed] [`Toml`].
   /// Provides parameters for compression format and pretty-print configuration (defaulting to off).
-  pub type CompressedToml<C, const PRETTY: bool = false> = crate::Compressed<C, Toml<PRETTY>>;
+  #[cfg_attr(docsrs, doc(cfg(feature = "compression")))]
+  #[cfg(feature = "compression")]
+  pub type CompressedToml<C, const PRETTY: bool = false> = crate::compression::Compressed<C, Toml<PRETTY>>;
 }
 
 /// Defines a [`CompressionFormat`] for the bzip compression algorithm.
@@ -417,7 +429,7 @@ pub mod toml_serde {
 pub mod bzip {
   pub extern crate bzip2;
 
-  use crate::{CompressionFormat, CompressionFormatLevels};
+  use crate::compression::{CompressionFormat, CompressionFormatLevels};
 
   use std::io::{Read, Write};
 
@@ -453,7 +465,7 @@ pub mod bzip {
 pub mod flate {
   pub extern crate flate2;
 
-  use crate::{CompressionFormat, CompressionFormatLevels};
+  use crate::compression::{CompressionFormat, CompressionFormatLevels};
 
   use std::io::{Read, Write};
 
@@ -539,7 +551,7 @@ pub mod flate {
 pub mod xz {
   pub extern crate xz2;
 
-  use crate::{CompressionFormat, CompressionFormatLevels};
+  use crate::compression::{CompressionFormat, CompressionFormatLevels};
 
   use std::io::{Read, Write};
 
