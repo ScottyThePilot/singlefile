1665d12d822a7408eb985c6fe150a4ae54e073dc
ScottyThePilot <scotty.codes@gmail.com>
2025-12-04 17:24:49 -0500

    Remove `UserError`

--- singlefile/src/error.rs
+++ singlefile/src/error.rs
@@ -23,18 +23,6 @@ pub enum Error<FE> {
   Other(#[from] OtherError)
 }
 
-#[allow(deprecated)]
-impl<FE> From<UserError<FE, Infallible>> for Error<FE> {
-  fn from(err: UserError<FE, Infallible>) -> Self {
-    match err {
-      UserError::Format(err) => Error::Format(err),
-      UserError::Io(err) => Error::Io(err),
-      UserError::Other(err) => Error::Other(err),
-      UserError::User(i) => match i {}
-    }
-  }
-}
-
 impl From<Error<io::Error>> for io::Error {
   fn from(err: Error<io::Error>) -> Self {
     match err {
@@ -81,53 +69,6 @@ impl<T> OrUserError<T, Infallible> {
 impl<T, U> From<T> for OrUserError<T, U> {
   fn from(err: T) -> Self {
     OrUserError::Base(err)
-  }
-}
-
-/// An error that can occur within `singlefile`, or an error from a user operation.
-#[deprecated]
-#[derive(Debug, Error)]
-pub enum UserError<FE, U> {
-  /// An error caused by an implementation of [`FileFormat`].
-  ///
-  /// [`FileFormat`]: crate::format::FileFormat
-  #[error("format error: {0}")]
-  Format(FE),
-  /// An error caused by the filesystem.
-  #[error(transparent)]
-  Io(#[from] io::Error),
-  /// Any other kind of error.
-  #[error(transparent)]
-  Other(#[from] OtherError),
-  /// An error caused by the user.
-  #[error("user error: {0}")]
-  User(U)
-}
-
-#[allow(deprecated)]
-impl<FE, U> UserError<FE, U> {
-  /// Maps this error into another error.
-  /// The new error type `E` must implement [`From<Error<FE>>`][enum@Error].
-  /// Additionally takes a closure allowing the user to manually convert the user error.
-  pub fn map_into<E, F>(self, f: F) -> E
-  where Error<FE>: Into<E>, F: FnOnce(U) -> E {
-    match self {
-      UserError::Format(err) => Error::Format(err).into(),
-      UserError::Io(err) => Error::Io(err).into(),
-      UserError::Other(err) => Error::Other(err).into(),
-      UserError::User(err) => f(err)
-    }
-  }
-}
-
-#[allow(deprecated)]
-impl<FE, U> From<Error<FE>> for UserError<FE, U> {
-  fn from(err: Error<FE>) -> Self {
-    match err {
-      Error::Format(err) => UserError::Format(err),
-      Error::Io(err) => UserError::Io(err),
-      Error::Other(err) => UserError::Other(err)
-    }
   }
 }
 
